02f92aaf496bfbe4b2abbdb77c27e10c50713f16
unknown <nikolajoksimovic95@gmail.com>
2022-11-29 13:52:42 +0100

    create user model

---
+++ models/user.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  user_id: {
+    type: String,
+  },
+  email: {
+    type: String,
+    require: [true, "Email must be provided."],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide valid email.",
+    ],
+    minlength: [6, "Plesse provide valid email(short)."],
+    unique: [true, "There is already an account registred to this email."],
+  },
+  password: {
+    type: String,
+    require: [true, "Password must be provided."],
+    minlength: [3, "Password too short. (Minimum length: 3)."],
+  },
+});
+
+// middleware
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// methods
+userSchema.methods.getJWToken = function () {
+  return jwt.sign(
+    { id: this.user_id, email: this.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+};
+userSchema.methods.comparePasswords = async function (potentialPassword) {
+  const isMatch = await bcrypt.compare(this.password, potentialPassword);
+  return isMatch;
+};
--- server.js
+++ server.js
@@ -2,19 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/connectDB");
 const cors = require("cors");
+
 const app = express();
+const homeRouter = require("./routes/home");
+const notFoundMiddleware = require("./middleware/notFound");
 
 app.use(cors());
 app.use(express.json());
+// app.use(express.static("public/build"));
 
 // routes
+app.use("/", homeRouter);
 
 // Cannot GET on refresh solution
-app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/build/index.html");
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(__dirname + "/public/build/index.html");
+// });
 
 // middleware
+app.use(notFoundMiddleware);
 
 // server
 const port = process.env.PORT || 8000;
