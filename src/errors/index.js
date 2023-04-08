16a67d671962e0648762f077fe7ab7c22a2fff00
unknown <nikolajoksimovic95@gmail.com>
2023-04-08 12:18:59 +0200

    removed src directory from git repository

--- src/errors/index.js
+++
@@ -1,5 +0,0 @@
-const CustomAPIError = require("./CustomAPIError");
-const BadRequestError = require("./BadRequestError");
-const AuthenticationError = require("./AuthenticationError");
-
-module.exports = { CustomAPIError, BadRequestError, AuthenticationError };
